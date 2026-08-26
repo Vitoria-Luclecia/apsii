76b2d1f2ff2cadc138a0d67cd178843307ebe97f
Vitoria Luclecia <lucleciavitoria@gmail.com>
2026-08-26 19:00:11 -0300

    atualização

--- Exercicio01/src/main/java/Exercicio01/Aniversariante.java
+++ Exercicio01/src/main/java/Exercicio01/Aniversariante.java
@@ -5,6 +5,15 @@ public class Aniversariante {
     private String nome;
     private DataAniversario dataAniversario;
 
+    public Aniversariante(String nome, int dia, int mes) {
+        this.nome = nome;
+        this.dataAniversario = new DataAniversario(dia, mes);
+    }
+
+    public Aniversariante(String nome, DataAniversario dataAniversario) {
+        this.nome = nome;
+        this.dataAniversario = dataAniversario;
+
     //gets e sets...
 
     @Override
